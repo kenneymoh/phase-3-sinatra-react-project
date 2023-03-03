90fe3360d742b4577ea507690743893431cf5c9f
kennedy <kenneyrit@gmail.com>
2023-03-03 13:10:41 +0300

    updated the  application controller

--- project-server/app/controllers/application_controller.rb
+++ project-server/app/controllers/application_controller.rb
@@ -1,9 +1,34 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
+
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/pets" do
+    pets = Pet.all
+    pets.to_json
+  end
+
+  get "/users" do
+    users = User.all
+    users.to_json
+  end
+
+  post "/login" do
+    user = User.find_by(email: params[:email], password: params[:password])
+    user.to_json
+  end
+
+  post "/new_pet" do
+    new_pet = Pet.create(
+      name: params[:name],
+      description: params[:description],
+      breed: params[:breed],
+      status: params[:status],
+      age: params[:age],
+      image_url: params[:image_url]
+
+    )
   end
 
 end
+
+
