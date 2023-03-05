5d91ca2640b15f0c91448f5a6d542ad989317863
kennedy <kenneyrit@gmail.com>
2023-03-05 22:41:35 +0300

    Updated the footer file

---
+++ project-client/pet-finder/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div classname= "footer">
+        <p> All rights reserved @Kenney 2023</p>
+    </div>
+  )
+}
+
+export default Footer
