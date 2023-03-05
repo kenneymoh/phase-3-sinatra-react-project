8cc35aa3d413246012ce31dab8c31a016bd184ca
kennedy <kenneyrit@gmail.com>
2023-03-05 22:33:15 +0300

    post methods added

--- project-server/app/controllers/application_controller.rb
+++ project-server/app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
   # Add your routes here
+  #gets all pets in the database
   get "/pets" do
     pets = Pet.all
     pets.to_json
@@ -12,9 +13,30 @@ class ApplicationController < Sinatra::Base
     users.to_json
   end
 
+  # post "/signup" do
+  #   #check fields are filled
+  #   if params[:name].blank? || params[:email].blank? || params[:password].blank?
+  #     redirect "/signup"
+
+  #     #check if username is taken
+  #   elsif user.find_by( name: params[user][:name])
+  #     redirect "/signup"
+  #   else
+  #     User.create(params[:user])
+  #   end
+  #   binding.pry
+
+  # end
+
+
+  get "/logout" do
+    redirect "/login"
+  end
+
   post "/login" do
     user = User.find_by(email: params[:email], password: params[:password])
     user.to_json
+    redirect "/"
   end
 
   post "/new_pet" do
