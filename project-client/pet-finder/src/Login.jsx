b4c6e7d35b79e5952fe89e11b8aa94201d4773e9
kennedy <kenneyrit@gmail.com>
2023-03-07 08:43:26 +0300

    state management in login

--- project-client/pet-finder/src/Login.jsx
+++ project-client/pet-finder/src/Login.jsx
@@ -1,38 +1,75 @@
-import React, { useState } from "react";
-import Button from 'react-bootstrap/Button';
+import './style.css'
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
+const SignIn = ({ setIsAuthenticated }) => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
-export const Login = (props) => {
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
-    const postUser = (email, pass)=> {
-        fetch("http://localhost:9292/users", {
-          method: "POST",
-          body: JSON.stringify({email:email,password:pass}),
-          headers: {
-            "Content-type": "application/json",
-            // Accept: "application.json"
-        }})
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://127.0.0.1:9292/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        setIsAuthenticated(true);
+        navigate("/allmemes");
+      } else {
+        const errorData = await response.json();
+        console.log(errorData);
       }
-
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(email);
+    } catch (error) {
+      console.error(error);
     }
+  };
 
-    return (
-        <div className="auth-form-container">
-            <h2>Login</h2>
-            <form className="login-form" onSubmit={handleSubmit}>
-                <label htmlFor="email">email</label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-                <label htmlFor="password">password</label>
-                <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-                <Button onClick={() => postUser()}> Login </Button>
-            </form>
-
-
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
         </div>
-    )
-}
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Sign In</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignIn;
